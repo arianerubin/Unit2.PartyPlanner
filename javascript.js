3dd2d53102eade57d3ff198a8d0e445cc84033dd
arianerubin <165211589+arianerubin@users.noreply.github.com>
2024-06-06 18:00:47 -0400

    notdoneatall

--- javascript.js
+++ javascript.js
@@ -16,30 +16,88 @@
 //Fetch= Fetch is used correctly to GET party data from the API, Fetch is used correctly to POST a new party to the API, Fetch is used correctly to DELETE a party from the API.
 //
 
-const init = () => {
-  parties();
-};
+const COHORT = "2405-FTB-ET-WEB-FT";
+const apiUrL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/2405-FTB-ET-WEB-FT/events`;
 
-const parties = async () => {
+const party = [];
+
+const partyList = document.querySelector("#party");
+const addPartyForm = document.querySelector("#addParty");
+addPartyForm.addEventListener("submit", addParty);
+
+/**
+ * Sync state with the API and rerender
+ */
+async function render() {
+  await getParty();
+  renderParty();
+}
+render();
+
+/**
+ * Update state with parties from API
+ */
+async function getParty() {
   try {
-    const data = await fetch(
-      "https://fsa-crud-2aa9294fe819.herokuapp.com/api/2405-FTB-ET-WEB-FT/events"
-    );
-    const response = await data.json();
-    const displayed = response.results;
-    return displayed;
+    const response = await fetch(API_URL);
+    const json = await response.json();
+    state.party = json.data;
   } catch (error) {
-    console.log(error.message);
+    console.error(error);
   }
-};
+}
 
-const addParty = async (event) => {
+/**
+ * Render parties from state
+ */
+function renderParty() {
+  if (!state.party.length) {
+    partyList.innerHTML = "<li>.</li>";
+    return;
+  }
+
+  const partyCards = state.party.map((party) => {
+    const li = document.createElement("li");
+    li.innerHTML = `
+        <h2>${party.name}</h2>
+        <p>${party.date}</p>
+        <p>${party.time}</p>
+        <p>${party.location}</p>
+        <p>${party.description}</p>
+    `;
+    return li;
+  });
+
+  partyList.replaceChildren(...partyCards);
+}
+
+/**
+ * Ask the API to create a new party based on form data
+ * @param {Event} event
+ */
+
+async function addParty(event) {
   event.preventDefault();
-  const name = document.getElementById("name");
-  const date = document.getElementById("date");
-  const time = document.getElementById("time");
-  const location = document.getElementById("location");
-  const description = document.getElementById("description");
-};
 
-init();
+  try {
+    const response = await fetch(API_URL, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        name: addPartyForm.name.value,
+        date: addPartyForm.date.value,
+        time: addPartyForm.time.value,
+        location: addPartyForm.location.value,
+        description: addPartyForm.description.value,
+      }),
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to add party");
+    }
+
+    render();
+  } catch (error) {
+    console.error(error);
+  }
+}
